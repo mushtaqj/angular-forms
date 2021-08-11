b6d99dd61d13df718fd968704e044ec1da3afa20
mushtaqj <mjmushtaq@gmail.com>
2021-08-11 11:43:05 +0530

    creates form and binds to formControl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  readonly loginFormControl = new FormControl();
+
+  ngOnInit(): void {
+    this.loginFormControl.valueChanges.subscribe(val => {
+      console.log(val);
+    });
+  }
 }
